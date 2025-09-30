7c0cbc19c77ce0d93d04edcfe2f19d5434620f3a
bazamel <basunako@gmail.com>
2025-09-30 17:31:28 +0700

    tested code

--- upload.py
+++ upload.py
@@ -1,19 +1,21 @@
 import gazu
 
-# Connect to your local Kitsu instance
 gazu.set_host("http://localhost/api")
 
-# Authenticate
 user = gazu.log_in("admin@example.com", "mysecretpassword")
 
-print("Logged in as:", user["first_name"])
+print("Logged in as:", user['user']['full_name'])
 
-tasks = gazu.task.all_tasks_for_asset(asset_dict)
-task_status = gazu.task.get_task_status_by_short_name("wip")
+projects = gazu.project.all_projects()
+
+assets = gazu.asset.all_assets_for_project(projects[0])
+
+tasks = gazu.task.all_tasks_for_asset(assets[0])
+task_status = gazu.task.get_task_status_by_short_name("todo")
 
 (comment, preview_file) = gazu.task.publish_preview(
-    task,
-    wip,
-    comment="Change status to work in progress",
+    tasks[0],
+    task_status,
+    comment="upload preview",
     preview_file_path="./thumbnail.png"
 )
